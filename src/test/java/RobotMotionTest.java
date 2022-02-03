a23b62fd1fed4e05f3019d277525eb5d76d65be0
Azoies1 <thomasle98@outlook.com>
2022-02-02 23:25:37 -0500

    Completed Moveforward test. Need to check test cases in the array for different robot positions

--- src/test/java/RobotMotionTest.java
+++ src/test/java/RobotMotionTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -20,6 +22,7 @@ class RobotMotionTest {
         robotMotion = new RobotMotion();
     }
 
+    @DisplayName("Getting argument from strings test")
     @ParameterizedTest()
     @MethodSource("arrayOfStrings")
     static void getArg(String[] c){
@@ -34,10 +37,12 @@ class RobotMotionTest {
         );
     }
 
+    @DisplayName("Retrieve array of strings from single string")
     @ParameterizedTest()
     @ValueSource(strings = {"U", "D", "r", "L", "M 4"})
     static void getStrings(String command){
-
+        char uppercase = Character.toUpperCase(command.charAt(0));
+        assertEquals(uppercase,RobotMotion.getStrings(command)[0].charAt(0));
     }
 
     @Test
@@ -50,6 +55,7 @@ class RobotMotionTest {
         assertFalse(RobotMotion.printCurrent());
     }
 
+    @DisplayName("Pen up test")
     @Test
     void penUp() {
         RobotMotion.robotPosition = new RobotPosition();
@@ -57,6 +63,7 @@ class RobotMotionTest {
         assertFalse(RobotMotion.robotPosition.getPenDown());
     }
 
+    @DisplayName("Pen down and trace test")
     @Test
     void penDown() {
         RobotMotion.robotPosition = new RobotPosition();
@@ -69,6 +76,7 @@ class RobotMotionTest {
         assertTrue(RobotMotion.roomArray.getRoomArray()[x][y]);
     }
 
+    @DisplayName("Turning left and right test")
     @Test
     void turnRightAndLeft() {
         RobotMotion.robotPosition = new RobotPosition();
@@ -86,28 +94,64 @@ class RobotMotionTest {
         assertEquals(oldDirection, RobotMotion.robotPosition.getDirection());
     }
 
+    @DisplayName("RobotMotion forward in given direction test")
     @Test
     void moveForward() {
         RobotMotion.robotPosition = new RobotPosition();
         RobotMotion.roomArray = new RoomArray(9);
 
+        //North PenDown = false
+        RobotMotion.moveForward(1);
+        assertFalse(RobotMotion.roomArray.getRoomArray()[0][1]);
+        assertEquals(0,RobotMotion.robotPosition.getPos_x());
+        assertEquals(1,RobotMotion.robotPosition.getPos_y());
 
+        //East PenDown = false
+        RobotMotion.turnRight();
+        RobotMotion.moveForward(2);
+        assertFalse(RobotMotion.roomArray.getRoomArray()[0][1]);
+        assertFalse(RobotMotion.roomArray.getRoomArray()[1][1]);
+        assertFalse(RobotMotion.roomArray.getRoomArray()[2][1]);
+        assertEquals(2,RobotMotion.robotPosition.getPos_x());
+        assertEquals(1,RobotMotion.robotPosition.getPos_y());
+
+        //South PenDown = true
+        RobotMotion.turnRight();
+        RobotMotion.penDown();
+        RobotMotion.moveForward(1);
+        assertFalse(RobotMotion.roomArray.getRoomArray()[2][1]);
+        assertTrue(RobotMotion.roomArray.getRoomArray()[2][0]);
+        assertEquals(2,RobotMotion.robotPosition.getPos_x());
+        assertEquals(0,RobotMotion.robotPosition.getPos_y());
+
+        //West PenDown = true
+        //moving off board
+        RobotMotion.turnRight();
+        RobotMotion.moveForward(3);
+        assertTrue(RobotMotion.roomArray.getRoomArray()[2][0]);
+        assertTrue(RobotMotion.roomArray.getRoomArray()[1][0]);
+        assertTrue(RobotMotion.roomArray.getRoomArray()[0][0]);
+        assertEquals(0,RobotMotion.robotPosition.getPos_x());
+        assertEquals(0,RobotMotion.robotPosition.getPos_y());
 
 
     }
 
+    @DisplayName("Print out array test")
     @Test
     void printArray() {
         RobotMotion.roomArray = new RoomArray(9);
         assertTrue(RobotMotion.printArray());
     }
 
+    @DisplayName("Print array with robot position test")
     @Test
     void printCurrent() {
         RobotMotion.robotPosition = new RobotPosition();
         assertTrue(RobotMotion.printCurrent());
     }
 
+    @DisplayName("Initializing robot system test")
     @Test
     void initializeSystem() {
         assertFalse(RobotMotion.initializeSystem(0));
@@ -118,13 +162,14 @@ class RobotMotionTest {
         //roomArray initialization
         int x,y,i;
         for (i = 0; i < 3; i++) {
-            x = (int) Math.random() * 3;
-            y = (int) Math.random() * 3;
+            x = (int) (Math.random() * 3);
+            y = (int) (Math.random() * 3);
             assertFalse(RobotMotion.roomArray.getRoomArray()[x][y]);
         }
     }
 
     @Test
+    @Disabled("Not sure to test RobotMotion help")
     void help() {
     }
 }
